c3f4b0bfc3645a213d3a4dfc3e0039750d076954
Cjay0917 <97030951+Cjay0917@users.noreply.github.com>
2022-05-19 03:20:53 +0800

    RequestForms

--- frontend/src/components/faculty/RequestedForm.js
+++ frontend/src/components/faculty/RequestedForm.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import axios from 'axios';
 import swal from 'sweetalert';
-import { useParams } from "react-router-dom";
+import "../../static/Faculty_Request.css";
+
 function ViewFaculty(props) {
-
-
     const [loading, setLoading] = useState(true);
     const [faculty, setStudents] = useState([]);
 
@@ -63,73 +62,60 @@ function ViewFaculty(props) {
         });
     }
 
-    if (loading) {
-        return <h4>Loading Student List Data...</h4>
-    }
-    else {
-        var faculty_HTMLTABLE = "";
+    var faculty_HTMLTABLE = "";
 
-        faculty_HTMLTABLE = faculty.map((item, index) => {
-            return (
-                <tr key={index}>
-                    <td>{item.name}</td>
-                    <td>{item.student_id}</td>
-                    <td>{item.subject_code}</td>
-                    <td>{item.semester}</td>
-                    <td>{item.school_year}</td>
-                    <td>{item.status}</td>
+    faculty_HTMLTABLE = faculty.map((item, index) => {
+        return (
+            <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.student_id}</td>
+                <td>{item.subject_code}</td>
+                <td>{item.semester}</td>
+                <td>{item.school_year}</td>
+                <td>{item.status}</td>
 
-                    <td className='text-center'>
-                        <button onClick={(e) => update(e, item.id)} className="btn btn-success btn-sm">Confirm</button>
-                        &nbsp;&nbsp;&nbsp;
-                        <button type="button" onClick={(e) => deleteStudent(e, item.id)} className="btn btn-danger btn-sm">Decline</button>
-                    </td>
-                </tr>
-            );
-        });
-    }
+                <td className='text-center'>
+                    <button onClick={(e) => update(e, item.id)} className="btn btn-success btn-sm btn-confirm">Confirm</button>
+                    &nbsp;&nbsp;&nbsp;
+                    <button type="button" onClick={(e) => deleteStudent(e, item.id)} className="btn btn-danger btn-sm btn-decline">Decline</button>
+                </td>
+            </tr>
+        );
+    });
 
     return (
-        <div>
-            <br />
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="card">
-                            <div className="card-header">
+        <div className="faculty-request">
+            <div className="fr-container-one">
+                <div className="fr-title">
+                    <ul>
+                        <li className="fr-info" id="title">Student Completion Request/s</li>
+                    </ul>
+                </div>
+                <div className="fr-line"></div>
+                <div className="fr-container-two">
+                    <div className="fr-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Student ID</th>
+                                    <th>Subject Code</th>
+                                    <th>Semester/Trimester/Summer</th>
+                                    <th>School Year</th>
+                                    <th>Status</th>
+                                    <th className='text-center'>Action</th>
+                                </tr>
+                            </thead>
 
-                                <h4>Requested Form Data
-
-                                </h4>
-                            </div>
-                            <div className="card-body">
-
-                                <table className="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Student ID</th>
-                                            <th>Subject Code</th>
-                                            <th>Semester/Trimester/Summer</th>
-                                            <th>School Year</th>
-                                            <th>Actions</th>
-                                            <th className='text-center'>Action</th>
-
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {faculty_HTMLTABLE}
-                                    </tbody>
-                                </table>
-
-                            </div>
-                        </div>
+                            <tbody>
+                                {faculty_HTMLTABLE}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-    );
-
+    )
 }
 
 export default ViewFaculty;
