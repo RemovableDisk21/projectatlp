b473052a1500620cce54873e82063e409cdc15a1
Cjay0917 <97030951+Cjay0917@users.noreply.github.com>
2022-05-19 21:57:09 +0800

    Navbar & Sidebar

--- frontend/src/layouts/student/Sidebar.js
+++ frontend/src/layouts/student/Sidebar.js
@@ -1,38 +1,46 @@
 import React from 'react';
-import { Link , useHistory} from 'react-router-dom';
-import swal from "sweetalert";
-
+import { Link, useHistory } from 'react-router-dom';
+import Swal from "sweetalert2";
 
 const Sidebar = () => {
     const history = useHistory();
-    const logoutSubmit = (e) =>{
+    const logoutSubmit = (e) => {
         e.preventDefault();
-                    localStorage.removeItem('auth_token');
-                    localStorage.removeItem('auth_name');
-                    localStorage.removeItem('auth_role');
-                    localStorage.removeItem('auth_status');
-                    swal("Succes", "Logged out Succesfully!","success");
-                    history.push('/login');
+        localStorage.removeItem('auth_token');
+        localStorage.removeItem('auth_name');
+        localStorage.removeItem('auth_role');
+        localStorage.removeItem('auth_status');
+
+        Swal.fire({
+            title: 'Confirm Log Out',
+            text: "Are you sure you want to log out?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirm'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                history.push('/login');
+            }
+        })
     }
-    return(
+
+    return (
         <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-
-        <div className="sb-sidenav-menu">
-            <div className="nav">
-                <Link className="nav-link-side" to="/student/dashboard"><a>Profile</a></Link>
-
-                <Link className="nav-link-side" to="/student/request"><a>Request Form</a></Link>
-                
-                <Link className="nav-link-side" to="/student/Formlist"><a>Requests</a></Link>
-
-                <Link className="nav-link-side" onClick={logoutSubmit} ><a>Logout</a></Link>
+            <div className="sb-sidenav-menu">
+                <div className="nav">
+                    <Link className="nav-link-side" to="/student/dashboard"><a>Profile</a></Link>
+                    <Link className="nav-link-side" to="/student/request"><a>Request Form</a></Link>
+                    <Link className="nav-link-side" to="/student/Formlist"><a>Requests</a></Link>
+                    <Link className="nav-link-side" onClick={logoutSubmit} ><a>Logout</a></Link>
+                </div>
             </div>
-        </div>
-        <div className="sb-sidenav-footer">
-            <div className="small">Logged in as:</div>
-            <p>Student</p>
-        </div>
-    </nav>
+            <div className="sb-sidenav-footer">
+                <div className="small">Logged in as:</div>
+                <p>Student</p>
+            </div>
+        </nav>
     );
 }
 
