8e08f198fad917ae0bdee1b60770c186b2311437
Cjay0917 <97030951+Cjay0917@users.noreply.github.com>
2022-05-19 16:37:12 +0800

    Student Profile

--- frontend/src/components/student/Dashboard.js
+++ frontend/src/components/student/Dashboard.js
@@ -1,22 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from 'axios';
-import swal from 'sweetalert';
-import { useState ,useEffect} from "react";
+import Swal from 'sweetalert2';
+import "../../static/Student_Profile.css";
 
-function Dashboard(){
-    
-    const  [ dashboardInput , setRegister] = useState([]);
+function Dashboard() {
+    const [dashboardInput, setRegister] = useState([]);
 
     useEffect(() => {
         const id = localStorage.getItem("auth_id");
-        axios.get(`/api/getprofile/${id}`).then(res=>{
-            if(res.status === 200)
-            {
-                console.log(res.data.profile);
+        axios.get(`/api/getprofile/${id}`).then(res => {
+            if (res.status === 200) {
                 setRegister(res.data.profile);
-                
-                //setLoading(false);
             }
         });
 
@@ -24,84 +19,86 @@ function Dashboard(){
 
     const handleInput = (e) => {
         e.persist();
-        setRegister({...dashboardInput, [e.target.name]: e.target.value});
+        setRegister({ ...dashboardInput, [e.target.name]: e.target.value });
     }
-   
+
     const dashboardSubmit = (e) => {
         e.preventDefault();
-   
         const data = {
             name: dashboardInput.name,
-            gender: dashboardInput.gender,
-            civil: dashboardInput.civil,
-            nationality: dashboardInput.nationality,
-            number: dashboardInput.number,
+            student_id: dashboardInput.student_id,
             course: dashboardInput.course,
             year: dashboardInput.year,
             section: dashboardInput.section,
-            student_id: dashboardInput.student_id,
+            number: dashboardInput.number,
             email: dashboardInput.email,
         }
-       console.log(data);
+
         const id = localStorage.getItem("auth_id");
-        axios.put(`/api/student_profile/${id}`, data).then(res =>{
-               if(res.data.status === 200)
-               {
-                  
-                   swal("Succes","Update Profile Settings Successfully","success");
-                   
-               }
-               else
-               {
-                   setRegister({... dashboardInput, error_list: res.data.validation_error});
-               }
+        axios.put(`/api/student_profile/${id}`, data).then(res => {
+            if (res.data.status === 200) {
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: 'Profile Updated',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
+            else {
+                setRegister({ ...dashboardInput, error_list: res.data.validation_error });
+            }
         });
-       
+
     }
 
-    return(
-        <div className="container rounded bg-white mt-5 mb-5">
-    <div className="row">
-        <div className="col-md-3 border-right">
-            <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"/><span className="font-weight-bold">Edogaru</span><span className="text-black-50">edogaru@mail.com.my</span><span> </span></div>
-        </div>
-        <div className="col-md-5 border-right">
-            <div className="p-3 py-5">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                    <h4 className="text-right">Profile Settings</h4>
+    return (
+        <div className="student-profile">
+            <div className="sp-container-one">
+                <div className="sp-title">
+                    <ul>
+                        <li className="sp-info" id="title">Student Profile</li>
+                    </ul>
                 </div>
-                <form onSubmit={dashboardSubmit}>
-                <div className="row mt-3">
-                    <div className="col-md-6"><label className="labels">Name</label>
-                    <input type="text" name ="name" onChange={handleInput} value = {dashboardInput.name} className="form-control" placeholder="first name" />
-                    </div>
-                   
+                <div className="sp-container-two">
+                    <form onSubmit={dashboardSubmit}>
+                        <div className="sp-form">
+                            <div className="sp-div">
+                                <label className="sp-label" id="sp-label-id" >Fullname:</label>
+                                <input className="sp-field" type="text" name="name" placeholder="Juan C. Dela Cruz" onChange={handleInput} value={dashboardInput.name} />
+                            </div>
+                            <div className="sp-div">
+                                <label className="sp-label">Student no:</label>
+                                <input className="sp-field" type="text" name="employee_id" onChange={handleInput} value={dashboardInput.student_id} disabled readOnly />
+                            </div>
+                            <div className="sp-div">
+                                <label className="sp-label">Course:</label>
+                                <input className="sp-field" type="text" name="course" onChange={handleInput} value={dashboardInput.course} />
+                            </div>
+                            <div className="sp-div">
+                                <label className="sp-label">Year:</label>
+                                <input className="sp-field" type="text" name="year" onChange={handleInput} value={dashboardInput.year} />
+                            </div>
+                            <div className="sp-div">
+                                <label className="sp-label">Section:</label>
+                                <input className="sp-field" type="text" name="section" onChange={handleInput} value={dashboardInput.section} />
+                            </div>
+                            <div className="sp-div">
+                                <label className="sp-label">Contact no:</label>
+                                <input className="sp-field" type="tel" name="number" maxLength="11" onChange={handleInput} value={dashboardInput.number} />
+                            </div>
+                            <div className="sp-div">
+                                <label className="sp-label">Email Address:</label>
+                                <input className="sp-field" type="email" name="email" onChange={handleInput} value={dashboardInput.email} disabled readOnly />
+                            </div>
+                            <div className="sp-div">
+                                <input className="sp-btn" type="submit" value="Update Profile" id="btn-update" />
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                
-                
-                <div className="row mt-3">
-                    <div className="col-md-12"><label className="labels">Gender</label><input type="text" name ="gender" className="form-control" onChange={handleInput} value = {dashboardInput.gender} placeholder="enter phone number"  /> </div>
-                    <div className="col-md-12"><label className="labels">Civil Status</label><input type="text" name ="civil" onChange={handleInput} value = {dashboardInput.civil}className="form-control" placeholder="enter address line 1"  />    </div>
-                    <div className="col-md-12"><label className="labels">Student ID:</label><input type="text" disabled name ="student_id"onChange={handleInput} value = {dashboardInput.student_id}className="form-control" placeholder="enter Student ID"  />  </div>
-                    <div className="col-md-12"><label className="labels">Nationality</label><input type="text" name ="nationality" onChange={handleInput} value = {dashboardInput.nationality} className="form-control" placeholder="enter address line 2"  /> </div>
-                    <div className="col-md-12"><label className="labels">Mobile Number</label><input type="text" name ="number" onChange={handleInput} value = {dashboardInput.number} className="form-control" placeholder="enter address line 2"  />  </div>
-                    <div className="col-md-12"><label className="labels">Email Address</label><input type="text" disabled name ="email" onChange={handleInput} value = {dashboardInput.email} className="form-control" placeholder="enter address line 2" /> </div>
-                    <div className="col-md-12"><label className="labels">Course</label><input type="text" name ="course" onChange={handleInput} value = {dashboardInput.course} className="form-control" placeholder="enter address line 2" /> </div>
-                </div>
-                <div className="row mt-3">
-                    <div className="col-md-6"><label className="labels">Year</label><input type="text"  name ="year" onChange={handleInput} value = {dashboardInput.year} className="form-control" placeholder="year" /> </div>
-                    <div className="col-md-6"><label className="labels">Section</label><input type="text" name ="section" onChange={handleInput} value = {dashboardInput.section} className="form-control"  placeholder="section
-                    "/></div>
-                </div>
-                <div className="mt-5 text-center"><button className="btn btn-primary profile-button" type="submit">Save Profile</button></div>
-                </form>
             </div>
         </div>
-
-    </div>
-</div>
-
-
     );
 }
 
