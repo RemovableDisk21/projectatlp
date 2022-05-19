33eab0b466db623f15a6431408dfee3610b2d03f
Albert-Jorda <92785696+Albert-Jorda@users.noreply.github.com>
2022-05-19 21:25:04 +0800

    Student UI

--- frontend/src/layouts/student/Navbar.js
+++ frontend/src/layouts/student/Navbar.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import React from "react";
 import { Link , useHistory} from "react-router-dom";
 import swal from "sweetalert";
+import logo1 from "../../static/images/bulsu.png";
+import logo2 from "../../static/images/cict.png";
+import "../../static/header.css";
 
 const Navbar = () => {
 const history = useHistory();
@@ -20,33 +23,17 @@ const logoutSubmit = (e) =>{
 }
 
     return(
-        <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            
-            <Link className="navbar-brand ps-3" to="/admin">Bulsu Completion System</Link>
-            
-            <button className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" to="/"><i className="fas fa-bars"></i></button>
-            
-            <form className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div className="input-group">
-                    <input className="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                    <button className="btn btn-primary" id="btnNavbarSearch" type="button"><i className="fas fa-search"></i></button>
+        <nav id="navbarrer" className="sb-topnav navbar navbar-expand navbarrer navbar-expand navbar-dark bg-dark">
+
+                <div class="logo-container">
+                    <img class="bulsu-logo" src={logo1} alt=""></img>
+                    <img class="cict-logo" src={logo2} alt=""></img>
                 </div>
-            </form>
-            
-                <button className="nav-link text-black" onClick={logoutSubmit} to="">Logout</button>
-            
-            <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li className="nav-item dropdown">
-                    <Link className="nav-link dropdown-toggle" id="navbarDropdown" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i className="fas fa-user fa-fw"></i></Link>
-                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><Link className="dropdown-item" to="#">Settings</Link></li>
-                        <li><Link className="dropdown-item" to="#">Activity Log</Link></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        
-                        <li><Link className="dropdown-item" to="#">Logout</Link></li>
-                    </ul>
-                </li>
-            </ul>
+
+                <div class="brand-container">
+                    <Link id="bulsu-brand" className="navbar-brand" to="/">Bulacan State University</Link>
+                    <p class="college-brand">College of Information and Communications Technology</p>
+                </div>
         </nav> 
     );
 }
